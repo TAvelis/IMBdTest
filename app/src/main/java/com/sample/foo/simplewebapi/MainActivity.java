62c3da87231871bc86ef772f9bc0247647ddffd4
TAvelis <avelitj12@juniata.edu>
2016-01-17 13:58:58 +0100

    Adaptation of project found on github, finds films based on title from OMDb's IMDb open API

--- app/src/main/java/com/sample/foo/simplewebapi/MainActivity.java
+++ app/src/main/java/com/sample/foo/simplewebapi/MainActivity.java
@@ -1,8 +1,8 @@
 package com.sample.foo.simplewebapi;
 
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,13 +21,13 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     EditText emailText;
     TextView responseView;
     ProgressBar progressBar;
     static final String API_KEY = "USE_YOUR_OWN_API_KEY";
-    static final String API_URL = "https://api.fullcontact.com/v2/person.json?";
+    static final String API_URL = "http://www.omdbapi.com/?";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
             // Do some validation here
 
             try {
-                URL url = new URL(API_URL + "email=" + email + "&apiKey=" + API_KEY);
+                URL url = new URL(API_URL + "s=" + email);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
